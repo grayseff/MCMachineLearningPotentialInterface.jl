522ae3200edc8f1a7582eb88b04f7fc8414e6a81
grayseff <ghun245@aucklanduni.ac.nz>
2023-05-19 11:10:21 +0200

    introduced threading for symm func calc

--- src/SymmFunc.jl
+++ src/SymmFunc.jl
@@ -144,7 +144,7 @@ Function to run over a vector of symmetry functions `total_symm_vec` and determi
 """
 function total_symm_calc(positions,dist2_mat,f_mat,total_symm_vec)
     g_mat = init_symm_vecs(dist2_mat,total_symm_vec)
-    for g_index in eachindex(total_symm_vec)
+    Threads.@threads for g_index in eachindex(total_symm_vec)
         g_mat[g_index,:] = calc_symm_vals!(positions,dist2_mat,f_mat,g_mat[g_index,:],total_symm_vec[g_index])
     end
     

3cb1a64b0fd627ad242fd28bfcd10d9e32e2bca2
grayseff <ghun245@aucklanduni.ac.nz>
2023-06-09 11:05:07 +0200

    foward pass implemented

--- src/MachineLearningPotential.jl
+++ src/MachineLearningPotential.jl
@@ -5,11 +5,13 @@ using Reexport
 include("Cutoff.jl")
 include("SymmFunc.jl")
 include("DeltaMatrix.jl")
+include("ForwardPass.jl")
 
 # Write your package code here.
 @reexport using .Cutoff
 @reexport using .SymmetryFunctions
 @reexport using .DeltaMatrix
+@reexport using .ForwardPass
 
 
 end
